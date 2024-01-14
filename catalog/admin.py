942a06054bc7fc199139c952707fc9280c3b93cc
Дмитрий <dfeskov1998@mail.ru>
2024-01-14 21:43:52 +0300

    register model Version

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Version
 
 
 @admin.register(Product)
@@ -11,3 +11,9 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'id',)
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('ver_product', 'ver_name', 'ver_number', 'id',)
+    search_fields = ('ver_product',)
+    filter = ('is_current_ver',)
